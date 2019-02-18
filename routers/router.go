adc0da5f9d28952e8b62b7223cc14efcee85efac
yfw <347097558@qq.com>
2019-02-18 21:37:49 +0800

    first commit

---
+++ routers/router.go
@@ -0,0 +1,28 @@
+package routers
+
+import (
+	"class1/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+	beego.Router("/abc", &controllers.MainController{})
+
+	beego.Router("/register", &controllers.MainController{})
+	//注意：当实现了自定义的请求方法，请求将不会访问默认方法
+	beego.Router("/login", &controllers.MainController{},"get:ShowLogin;post:HandleLogin")
+	beego.Router("/index",&controllers.MainController{},"get:ShowIndex")
+
+	beego.Router("/addArticle",&controllers.MainController{},"get:ShowAdd;post:HandleAdd")
+
+	beego.Router("/content",&controllers.MainController{},"get:ShowContent")
+
+	beego.Router("/update",&controllers.MainController{},"get:ShowUpdate;post:HandleUpdate")
+
+	beego.Router("/delete",&controllers.MainController{},"get:HandleDelete")
+
+	beego.Router("/addType",&controllers.MainController{},"get:ShowAddType;post:HandleAddType")
+	beego.Router("/deleteType",&controllers.MainController{},"get:HandleTypeDelete")
+	beego.Router("/logout", &controllers.MainController{},"get:Logout")
+}
